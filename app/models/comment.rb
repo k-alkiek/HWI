627e626d3c96aa6ba781f11f307537e2409677a0
Khaled Barie <k.alkiek@gmail.com>
2015-08-21 19:13:51 +0200

    added missing colons in models

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,4 @@
 class Comment < ActiveRecord::Base
-	belongs_to :user ,dependent :destroy
-	belongs_to :review , dependent :destroy
+	belongs_to :user ,dependent: :destroy
+	belongs_to :review , dependent: :destroy
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,6 +1,6 @@
 class Review < ActiveRecord::Base
 	has_many :comments
 	
-	belongs_to :item , dependent :destroy
+	belongs_to :item , dependent: :destroy
 	
 end
