991d1048e1c341744da5bee19c4be9bb42cea294
Khaled Barie <k.alkiek@gmail.com>
2015-08-31 01:36:34 +0200

    cleaned the project

--- app/models/like.rb
+++
@@ -1,6 +0,0 @@
-class Like < ActiveRecord::Base
-	belongs_to :user
-	belongs_to :review
-	validates :post_id , presence = true
-	validates :user_id , presence = true
-end
--- app/views/likes/show.json.jbuilder
+++
@@ -1,1 +0,0 @@
-json.extract! @like, :id, :user_id, :review_id, :created_at, :updated_at
